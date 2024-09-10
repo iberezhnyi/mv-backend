bc9d06894fe3b35218b91449766d0fe433d7140e
Ihor Berezhnyi <iberezhnyi@gmail.com>
2024-09-10 17:51:25 +0300

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(cookieParser())
 
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:5173', 'https://iberezhnyi.github.io/planner'],
+    origin: ['http://localhost:5173', 'https://iberezhnyi.github.io'],
     // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }
